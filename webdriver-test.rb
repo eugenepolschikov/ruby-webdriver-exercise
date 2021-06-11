da8c8c8d6800a597839dcfd0b6ab1209cb45c461
eugene polschikov <eugene.polschikov@gmail.com>
2021-06-12 01:04:06 +0300

    Enhancing primary test flow

--- webdriver-test.rb
+++ webdriver-test.rb
@@ -5,20 +5,27 @@ require 'json'
 
 class SeleniumDemoTests < Test::Unit::TestCase
 
-  SEARCH_QUERY = "web"
+  SEARCH_QUERY = ARGV[0]
   WAIT_INTERVAL_DEFAULT = 10
+  ARGS_NUM = ARGV.length
 
   def test_end_to_end_test_flow
 
     page_objs = File.read('./page-objects.json')
     data_hash = JSON.parse(page_objs)
 
+    if ARGS_NUM > 1 || ARGS_NUM == 0
+      puts "invalid args number"
+      exit
+    end
+    puts "Working on #{SEARCH_QUERY}"
+
     # driver = Selenium::WebDriver.for :chrome
     driver = Selenium::WebDriver.for :chrome, switches: %w[--incognito]
     driver.manage.delete_all_cookies
     driver.manage.window.maximize
     driver.navigate.to "http://www.upwork.com"
-    # sleep 65
+    # sleep 165
     wait = Selenium::WebDriver::Wait.new(:timeout => WAIT_INTERVAL_DEFAULT) # seconds
     puts "Page title is #{driver.title}"
     search_freelancers_agencies_input = wait.until { driver.find_element(:name, data_hash['landing']['searchInputName']) }
@@ -29,7 +36,7 @@ class SeleniumDemoTests < Test::Unit::TestCase
     magnifying_glass_button.click
 
     wait_second = Selenium::WebDriver::Wait.new(:timeout => WAIT_INTERVAL_DEFAULT) # seconds
-    wait_second.until { driver.title.downcase.start_with? "web" }
+    wait_second.until { driver.title.downcase.start_with? SEARCH_QUERY }
     puts "Page title is #{driver.title}"
     driver.quit
 
