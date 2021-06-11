b309be40155c36639809e5b70a7542cc2edd3a05
eugene polschikov <eugene.polschikov@gmail.com>
2021-06-12 00:06:16 +0300

    enhanced webdriver code.

--- webdriver-test.rb
+++ webdriver-test.rb
@@ -11,21 +11,20 @@ class SeleniumDemoTests  < Test::Unit::TestCase
     # driver = Selenium::WebDriver.for :chrome
     driver = Selenium::WebDriver.for :chrome, switches: %w[--incognito]
     driver.manage.delete_all_cookies
-    driver.navigate.to "http://www.upwork.com"
     driver.manage.window.maximize
+    driver.navigate.to "http://www.upwork.com"
+    # sleep 65
     wait = Selenium::WebDriver::Wait.new(:timeout => 10) # seconds
     puts "Page title is #{driver.title}"
-    puts "test debug"
-    puts "test debug"
     search_freelancers_agencies_input = wait.until { driver.find_element(:name, "q") }
     search_freelancers_agencies_input.click
-    #### search_freelancers_agencies_input = driver.find_element(:name, "q")
+
     search_freelancers_agencies_input.send_keys(SEARCH_QUERY)
     magnifying_glass_button = wait.until { driver.find_element(:css, "button[type='submit'][aria-label='Search']") }
     magnifying_glass_button.click
 
-    magnifying_glass_button.submit
-    wait.until { driver.title.downcase.start_with? "web - Upwork" }
+    wait_second = Selenium::WebDriver::Wait.new(:timeout => 10) # seconds
+    wait_second.until { driver.title.downcase.start_with? "web" }
     puts "Page title is #{driver.title}"
     driver.quit
 
