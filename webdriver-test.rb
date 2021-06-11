d8037a2a19ab7ad50d0d539503c76e81a8da8944
eugene polschikov <eugene.polschikov@gmail.com>
2021-06-12 00:46:59 +0300

    Introduced page-object in solution

--- webdriver-test.rb
+++ webdriver-test.rb
@@ -1,12 +1,17 @@
 require 'rubygems'
 require 'selenium-webdriver'
 require 'test/unit'
+require 'json'
 
-class SeleniumDemoTests  < Test::Unit::TestCase
+class SeleniumDemoTests < Test::Unit::TestCase
 
   SEARCH_QUERY = "web"
+  WAIT_INTERVAL_DEFAULT = 10
 
   def test_end_to_end_test_flow
+
+    page_objs = File.read('./page-objects.json')
+    data_hash = JSON.parse(page_objs)
 
     # driver = Selenium::WebDriver.for :chrome
     driver = Selenium::WebDriver.for :chrome, switches: %w[--incognito]
@@ -14,16 +19,16 @@ class SeleniumDemoTests  < Test::Unit::TestCase
     driver.manage.window.maximize
     driver.navigate.to "http://www.upwork.com"
     # sleep 65
-    wait = Selenium::WebDriver::Wait.new(:timeout => 10) # seconds
+    wait = Selenium::WebDriver::Wait.new(:timeout => WAIT_INTERVAL_DEFAULT) # seconds
     puts "Page title is #{driver.title}"
-    search_freelancers_agencies_input = wait.until { driver.find_element(:name, "q") }
+    search_freelancers_agencies_input = wait.until { driver.find_element(:name, data_hash['landing']['searchInputName']) }
     search_freelancers_agencies_input.click
 
     search_freelancers_agencies_input.send_keys(SEARCH_QUERY)
-    magnifying_glass_button = wait.until { driver.find_element(:css, "button[type='submit'][aria-label='Search']") }
+    magnifying_glass_button = wait.until { driver.find_element(:css, data_hash['landing']['magnifyingGlassButtonCss']) }
     magnifying_glass_button.click
 
-    wait_second = Selenium::WebDriver::Wait.new(:timeout => 10) # seconds
+    wait_second = Selenium::WebDriver::Wait.new(:timeout => WAIT_INTERVAL_DEFAULT) # seconds
     wait_second.until { driver.title.downcase.start_with? "web" }
     puts "Page title is #{driver.title}"
     driver.quit
