54ce49d7396536c88cafc5f6f143bc6a28606ac0
eugene polschikov <eugene.polschikov@gmail.com>
2021-06-12 02:54:09 +0300

    introduced setup and teardown.

--- webdriver-test.rb
+++ webdriver-test.rb
@@ -21,23 +21,23 @@ class SeleniumDemoTests < Test::Unit::TestCase
     #
     # creating chrome instance on default settings/capabilities.
     # driver = Selenium::WebDriver.for :chrome
-    driver = Selenium::WebDriver.for :chrome, switches: %w[--incognito]
-    driver.manage.delete_all_cookies
-    driver.manage.window.maximize
+    @driver = Selenium::WebDriver.for :chrome, switches: %w[--incognito]
+    @driver.manage.delete_all_cookies
+    @driver.manage.window.maximize
     puts "opening website '#{BASE_URL}'"
-    driver.navigate.to "#{BASE_URL}"
+    @driver.navigate.to "#{BASE_URL}"
+    @page_objs = File.read('./page-objects.json')
+    @data_hash = JSON.parse(@page_objs)
+
     # sleep 165
   end
 
   #### Closing browser after each test
   def teardown
-    @browser.quit
+    @driver.quit
   end
 
   def test_end_to_end_test_flow
-
-    page_objs = File.read('./page-objects.json')
-    data_hash = JSON.parse(page_objs)
 
     if ARGS_NUM > 1 || ARGS_NUM == 0
       puts "invalid args number"
@@ -46,25 +46,25 @@ class SeleniumDemoTests < Test::Unit::TestCase
     puts "Working on #{SEARCH_QUERY}"
 
     wait = Selenium::WebDriver::Wait.new(:timeout => WAIT_INTERVAL_DEFAULT) # seconds
-    puts "wait for the element with locator '#{data_hash['landing']['searchInputName']}' on the page '#{driver.current_url}'"
-    search_input = wait.until { driver.find_element(:name, data_hash['landing']['searchInputName']) }
-    puts "clicking on the element with locator '#{data_hash['landing']['searchInputName']}' on the page '#{driver.current_url}'"
+    puts "wait for the element with locator '#{@data_hash['landing']['searchInputName']}' on the page '#{@driver.current_url}'"
+    search_input = wait.until { @driver.find_element(:name, @data_hash['landing']['searchInputName']) }
+    puts "clicking on the element with locator '#{@data_hash['landing']['searchInputName']}' on the page '#{@driver.current_url}'"
     search_input.click
 
-    puts "entering '#{SEARCH_QUERY}' in search input defined by locator '#{data_hash['landing']['searchInputName']}' on the page '#{driver.current_url}'"
+    puts "entering '#{SEARCH_QUERY}' in search input defined by locator '#{@data_hash['landing']['searchInputName']}' on the page '#{@driver.current_url}'"
     search_input.send_keys(SEARCH_QUERY)
-    magnifying_glass_button = wait.until { driver.find_element(:css, data_hash['landing']['magnifyingGlassButtonCss']) }
-    puts "clicking on magnifying glass button defined by locator '#{data_hash['landing']['magnifyingGlassButtonCss']}' on the page '#{driver.current_url}'"
+    magnifying_glass_button = wait.until { @driver.find_element(:css, @data_hash['landing']['magnifyingGlassButtonCss']) }
+    puts "clicking on magnifying glass button defined by locator '#{@data_hash['landing']['magnifyingGlassButtonCss']}' on the page '#{@driver.current_url}'"
     magnifying_glass_button.click
 
     wait_second = Selenium::WebDriver::Wait.new(:timeout => WAIT_INTERVAL_DEFAULT) # seconds
-    wait_second.until { driver.title.downcase.start_with? SEARCH_QUERY }
-    puts "checking that after clicking magnifying glass button - actual page title '#{driver.title}' starts with '#{SEARCH_QUERY}'"
+    wait_second.until { @driver.title.downcase.start_with? SEARCH_QUERY }
+    puts "checking that after clicking magnifying glass button - actual page title '#{@driver.title}' starts with '#{SEARCH_QUERY}'"
 
     # extracting freelancers from this first page.
     freelancers = Hash.new()
-    freelances_names = driver.find_elements(:css, 'div[data-qa-freelancer-ciphertext] button[itemprop=name]')
-    freelancer_titles = driver.find_elements(:css, 'div[data-qa-freelancer-ciphertext] p.freelancer-title')
+    freelances_names = @driver.find_elements(:css, 'div[data-qa-freelancer-ciphertext] button[itemprop=name]')
+    freelancer_titles = @driver.find_elements(:css, 'div[data-qa-freelancer-ciphertext] p.freelancer-title')
     freelances_names.zip(freelancer_titles).each do |name, title|
       puts "NAME:'#{name.text}'  TITLE: '#{title.text}'"
       freelancers[name.text] = title.text
