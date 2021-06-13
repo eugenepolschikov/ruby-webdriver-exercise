5a65ff93fc8d025af81b602175d5491822f96884
eugene polschikov <eugene.polschikov@gmail.com>
2021-06-13 10:18:34 +0300

    implemented clicking on random freelancer title

--- webdriver-test.rb
+++ webdriver-test.rb
@@ -29,6 +29,7 @@ class SeleniumDemoTests < Test::Unit::TestCase
     @page_objs = File.read('./page-objects.json')
     @data_hash = JSON.parse(@page_objs)
 
+    #this extra sleep is needed if captcha needs to be handled manually
     # sleep 165
   end
 
@@ -57,8 +58,9 @@ class SeleniumDemoTests < Test::Unit::TestCase
     puts "clicking on magnifying glass button defined by locator '#{@data_hash['landing']['magnifyingGlassButtonCss']}' on the page '#{@driver.current_url}'"
     magnifying_glass_button.click
 
-    wait_second = Selenium::WebDriver::Wait.new(:timeout => WAIT_INTERVAL_DEFAULT) # seconds
-    wait_second.until { @driver.title.downcase.start_with? SEARCH_QUERY }
+    # wait_second = Selenium::WebDriver::Wait.new(:timeout => WAIT_INTERVAL_DEFAULT) # seconds
+    # wait_second.until { @driver.title.downcase.start_with? SEARCH_QUERY }
+    wait.until { @driver.title.downcase.start_with? SEARCH_QUERY }
     puts "checking that after clicking magnifying glass button - actual page title '#{@driver.title}' starts with '#{SEARCH_QUERY}'"
 
     # extracting freelancers from this first page.
@@ -66,17 +68,47 @@ class SeleniumDemoTests < Test::Unit::TestCase
     freelances_names = @driver.find_elements(:css, 'div[data-qa-freelancer-ciphertext] button[itemprop=name]')
     freelancer_titles = @driver.find_elements(:css, 'div[data-qa-freelancer-ciphertext] p.freelancer-title')
     freelancer_overview = @driver.find_elements(:css, 'p.overview')
+    freelanceer_skills = @driver.find_elements(:xpath, '//div[@class="up-skill-wrapper"][./div[contains(@class,up-skill-badge)]]')
     count = 0
+    puts "analyzing found freelancers and checking for keyword '#{SEARCH_QUERY}' in all found freelancers' title, overview, skills"
 
-    freelancer_titles.zip(freelancer_overview).each do |title, overview|
-      puts "title:'#{title.text}'  overview: '#{overview.text}'"
+    freelancer_titles.zip(freelancer_overview, freelanceer_skills).each do |title, overview, skill|
+      # these two lines are for debugging. @TODO remove before creating a PR
+      # puts "title:'#{title.text}'  overview: '#{overview.text}'"
       # freelancers[name.text] = title.text
-      freelancers[freelances_names[count].text] = [title.text, overview.text]
+      freelancers[freelances_names[count].text] = [title.text, overview.text, skill.text]
+
+      if title.text.downcase.include? SEARCH_QUERY.downcase
+        puts "fl '#{freelances_names[count].text}' title contains '#{SEARCH_QUERY}' keyword"
+      else
+        puts "fl '#{freelances_names[count].text}' title DOES NOT contain '#{SEARCH_QUERY}' keyword"
+      end
+
+      if overview.text.downcase.include? SEARCH_QUERY.downcase
+        puts "fl '#{freelances_names[count].text}' overview contains '#{SEARCH_QUERY}' keyword"
+      else
+        puts "fl '#{freelances_names[count].text}' overview DOES NOT contain '#{SEARCH_QUERY}' keyword"
+      end
+
+      if skill.text.downcase.include? SEARCH_QUERY.downcase
+        puts "fl '#{freelances_names[count].text}' skill contains '#{SEARCH_QUERY}' keyword"
+      else
+        puts "fl '#{freelances_names[count].text}' skill DOES NOT contain '#{SEARCH_QUERY}' keyword"
+      end
+
       count += 1
     end
 
-    puts "############"
+    puts "######## extracted #{count} freelancers ########"
     puts freelancers
+    puts "#######"
+
+    puts "clicking on random freelancer title"
+    freelancer_titles.sample.click
+    puts "wait for freelancer details page/widget opens"
+    element = wait.until { @driver.find_element(:css => "div[tabindex='-1'][class='up-slider-content']") }
+
+    sleep 5
 
     # driver.find_elements(:css, 'div[data-qa-freelancer-ciphertext]').each do |div|
     #   # puts div.attribute_value("data-channel")
